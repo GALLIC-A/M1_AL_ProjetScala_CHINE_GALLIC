c61f1e70c2c090a7fdf1b56cbad8dff6cbf31341
GALLIC-A <axel.gallic@orange.fr>
2024-05-25 11:48:06 +0200

    Exercices 6 et 7 :  - Création de la case class WikiPage contenant deux paramètres title et words  - Création de la fonction parseJson  - Adaptation de la fonction run() en conséquences

--- wikipages/src/main/scala/Main.scala
+++ wikipages/src/main/scala/Main.scala
@@ -1,7 +1,10 @@
 import scalaj.http.Http
 import scopt.OParser
+import play.api.libs.json.{JsArray, Json}
 
 case class Config(limit: Int = 10, keyword: String = "")
+
+case class WikiPage(title: String, words: Int)
 
 object Main extends App {
   parseArguments(args) match {
@@ -31,10 +34,16 @@ object Main extends App {
 
   def run(config: Config): Unit = {
     // println(config)
+
     val url = formatUrl(config.keyword, config.limit)
     getPages(url) match {
       case Left(errorCode) => println(s"Une erreur est survenue : $errorCode")
-      case Right(body)     => println(body)
+      case Right(body) =>
+        val pages = parseJson(body)
+        println(s"Nombre de pages trouvées : ${pages.length}")
+        pages.foreach(page =>
+          println(s"Titre : ${page.title}, Nombre de mots : ${page.words}")
+        )
     }
   }
 
@@ -48,6 +57,17 @@ object Main extends App {
       Left(result.code)
     } else {
       Right(result.body)
+    }
+  }
+
+  def parseJson(jsonString: String): Seq[WikiPage] = {
+    val jsonDatas = Json.parse(jsonString)
+    val searchResults = (jsonDatas \ "query" \ "search").as[JsArray]
+
+    searchResults.value.map { result =>
+      val title = (result \ "title").as[String]
+      val words = (result \ "wordcount").as[Int]
+      WikiPage(title, words)
     }
   }
 }
